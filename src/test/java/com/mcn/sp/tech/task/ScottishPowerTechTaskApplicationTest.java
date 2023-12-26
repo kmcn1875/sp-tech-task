aad191e2530c78424f00691165521db810e80c09
Keith McNeill <k.mcneill@blackarrowgroup.io>
2023-12-26 20:14:38 +0000

    Added experimental test container

--- src/test/java/com/mcn/sp/tech/task/ScottishPowerTechTaskApplicationTest.java
+++ src/test/java/com/mcn/sp/tech/task/ScottishPowerTechTaskApplicationTest.java
@@ -1,13 +1,49 @@
 package com.mcn.sp.tech.task;
 
+import com.mcn.sp.tech.task.repo.MeterReadingsRepo;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ScottishPowerTechTaskApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:15-alpine"
+    ).withUsername("sa").withPassword("sa");
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Autowired
+    private MeterReadingsRepo meterReadingsRepo;
+
+    @Test
+    void contextLoads() {
+        assertThat(meterReadingsRepo.count()).isEqualTo(0L);
+    }
 
 }
